7d6a6c8edc9062c8f8b2694d200fd4c8b2ad878d
francistopher <francistopherrr@outlook.com>
2022-10-05 20:20:01 -0400

    CREATE Quick Sort Repo

---
+++ sorting/quick/quick.js
@@ -0,0 +1,28 @@
+/*
+ *	Class that implements the merge 
+ * 	sort algorithm to sort the nodes
+ */
+
+class Quick extends NodeMaster {
+	
+	constructor() {
+		super()
+		this.sortButton.addEventListener("click", () => {
+			this.sortButton.style.display = "none"
+			this.#sort(0, this.stage.length - 1)
+		})
+	}
+
+	// outer loop
+	#sort(leftIndex, rightIndex) {
+		if (leftIndex >= rightIndex) {
+			return;
+		}
+		var middleIndex = parseInt((rightIndex - 1) / 2)
+		console.log("let's merge sort!")
+		// i feel sleepy, have recovered, back in town
+	}
+
+
+
+}
