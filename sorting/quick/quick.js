4ab705eefe1e9e3c656bb15c92162ba814d32166
francistopher <francistopherrr@outlook.com>
2023-02-14 21:08:21 -0500

    UPDATE quick.js with basic quick sort animation

--- sorting/quick/quick.js
+++ sorting/quick/quick.js
@@ -4,6 +4,34 @@
  */
 
 class Quick extends NodeMaster {
+
+    #showShuffleIfSorted()
+    {
+        setTimeout(()=> {
+            var isSorted = true;
+            for (var i = 0, prev = -1; i < this.stage.length; i++)
+            {
+                if (prev < this.stage[i]["node"].value)
+                {
+                    prev = this.stage[i]["node"].value;
+                }
+                else
+                {
+                    isSorted = false;
+                    break;
+                }
+            }
+            if (isSorted)
+            {
+                this.shuffleButton.style.display = "inline"; 
+            }
+            else
+            {
+                this.#showShuffleIfSorted();
+            }
+        }, 2000);
+    }
+
     constructor() {
         super();
         this.sortButton.addEventListener("click", () => {
@@ -11,6 +39,7 @@ class Quick extends NodeMaster {
             console.log(this.stage);
             this.#sort(0, this.stage.length - 1, this.stage.length * 1000);
             console.log(this.stage);
+            this.#showShuffleIfSorted();
         });
     }
 
@@ -22,7 +51,7 @@ class Quick extends NodeMaster {
         this.stage[i]["node"].setPosition(this.stage[i]["x"], "vw", "", "");
     }
 
-    #partition(leftIndex, rightIndex) {
+    #partition(leftIndex, rightIndex, ticks) {
         // node at the rightmost index is the pivot
         let pivot = this.stage[rightIndex]["node"];
         let i = leftIndex - 1;
@@ -32,15 +61,32 @@ class Quick extends NodeMaster {
                 this.#swap(i, j);
             }
         }
-        this.#swap(i + 1, rightIndex);
+        for (let j = leftIndex; j <= rightIndex - 1; j++)
+        {
+            this.stage[j]["node"].setBackgroundColor('orange');
+        }
+        pivot.setBackgroundColor('yellow')
+        setTimeout(()=> {
+            for (var j = leftIndex; j < rightIndex; j++)
+            {   
+                this.stage[j]["node"].setBackgroundColor('white');
+            }
+            pivot.setBackgroundColor('white');
+            this.#swap(i + 1, rightIndex);
+            var sorted = true;
+        }, ticks);
         return i + 1;
     }
 
+
     #sort(leftIndex, rightIndex, ticks) {
         if (leftIndex < rightIndex) {
-            let parti = this.#partition(leftIndex, rightIndex);
-            this.#sort(leftIndex, parti - 1, ticks);
-            this.#sort(parti + 1, rightIndex, ticks);
+            let parti = this.#partition(leftIndex, rightIndex, ticks/1.5);
+            setTimeout(()=> {
+                this.#sort(leftIndex, parti - 1, ticks / 1.5);
+                this.#sort(parti + 1, rightIndex, ticks / 1.5);
+            }, ticks / 1.5);
         }
+
     }
 }
