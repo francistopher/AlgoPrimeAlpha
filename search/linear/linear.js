5acf6ac413f328ef8202a2d9faeae24ef20f8ed8
francistopher <francistopherrr@outlook.com>
2023-09-28 12:13:08 -0400

    UPDATE linear.js to display both the shuffle and search button if the user doesn't want to shuffle before searching

--- search/linear/linear.js
+++ search/linear/linear.js
@@ -3,19 +3,19 @@
  * via the bubble algorithm
  */
 class Linear extends NodeMaster {
-	constructor() {
-		super();
-		console.log("LINEAR");
-        this.sortButton.innerHTML = "SEARCH";
-	this.sortButton.addEventListener("click", ()=> {
-			this.sortButton.style.display = "none";
-			this.#search(0, 0, "", 0);
-		});
-	}
+   constructor() {
+      super();
+      this.sortButton.innerHTML = "SEARCH";
+      this.createSearchField();
+      this.sortButton.style.display = "block";
+      this.sortButton.addEventListener("click", () => {
+         this.sortButton.style.display = "none";
+         this.#search(0, 0, "", 0);
+      });
+   }
 
-// outer loop
-#search(i, j, temp, ticks) {
-    console.log("to be continued");
-}
-
+   // outer loop
+   #search(i, j, temp, ticks) {
+      console.log("to be continued");
+   }
 }
