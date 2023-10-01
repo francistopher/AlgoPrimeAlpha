8b8e0f7a2ffba16656a67b8df4d2fa229d22c344
francistopher <francistopherrr@outlook.com>
2023-09-30 22:31:17 -0400

    UPDATE node.js with autoformat

--- node.js
+++ node.js
@@ -3,56 +3,49 @@
  *  which would serve as a member of an ensemble to perform
  * 	an algorithm.
  */
-class Node 
-{
+class Node {
+   constructor(value) {
+      this.value = value - 1;
+      this.label;
+      this.x;
+      this.y;
+      this.#buildLabel();
+   }
 
-	constructor(value) 
-	{
-		this.value = value - 1;
-		this.label;
-		this.x;
-		this.y;
-		this.#buildLabel();
-	}
+   // creates label element with id and inner html value
+   #buildLabel() {
+      this.label = document.createElement("LABEL");
+      this.label.id = "node";
+      this.label.innerHTML = this.value + 1;
+   }
 
-	// creates label element with id and inner html value
-	#buildLabel() 
-	{
-		this.label = document.createElement("LABEL");
-		this.label.id = "node";
-		this.label.innerHTML = this.value + 1;
-	}
+   remove() {
+      this.label.remove();
+   }
 
-	remove()
-	{
-		this.label.remove();
-	}
+   setBackgroundColor(color) {
+      this.label.style.backgroundColor = color;
+   }
 
-	setBackgroundColor(color) 
-	{
-		this.label.style.backgroundColor = color;
-	}
+   setBorderColor(color) {
+      this.label.style.borderColor = color;
+   }
 
-	setBorderColor(color)
-	{
-		this.label.style.borderColor = color;
-	}
-	
-	/*
-	 *  Set position of label given
-	 *	{x} x coordinate value
-	 * 	{xu} x coordinate units
-	 *  {y} y coordinate value
-	 *	{yu} : y coordinate units
-	 */
-	setPosition(x, xu, y, yu) {
-		this.x = x;
-		this.label.style.left = this.x + xu;
-		// only applies y coordinate if it's given
-		if (y != "") {
-			this.y = y;
-			this.label.style.top = this.y + yu;
-		}
-		document.body.appendChild(this.label); // serves a special purpose
-	}
+   /*
+    *  Set position of label given
+    *	{x} x coordinate value
+    * 	{xu} x coordinate units
+    *  {y} y coordinate value
+    *	{yu} : y coordinate units
+    */
+   setPosition(x, xu, y, yu) {
+      this.x = x;
+      this.label.style.left = this.x + xu;
+      // only applies y coordinate if it's given
+      if (y != "") {
+         this.y = y;
+         this.label.style.top = this.y + yu;
+      }
+      document.body.appendChild(this.label); // serves a special purpose
+   }
 }
