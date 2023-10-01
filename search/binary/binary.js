deae3d181584b0382cf9198f7fdc257da3ee7e93
francistopher <francistopherrr@outlook.com>
2023-09-30 22:51:49 -0400

    UPDATE binary.js with visualizations with coloring

--- search/binary/binary.js
+++ search/binary/binary.js
@@ -8,26 +8,40 @@ class Binary extends NodeMaster {
       this.createSearchField();
       this.sortButton.addEventListener("click", () => {
          if (!this.checkForValue()) {
-            console.log("index", this.#binarySearch(0, this.stage.length));
+            this.#binarySearch(0, this.stage.length);
          }
       });
    }
 
    #binarySearch(startIndex, endIndex) {
-      console.log(startIndex, endIndex);
+      //console.log(startIndex, endIndex);
       if (endIndex > startIndex) {
          const mid = Math.floor(startIndex + (endIndex - startIndex) / 2);
          //console.log(mid);
          const midElement = this.stage[mid]["node"];
          //console.log(midElement);
-         if (midElement.value + 1 == this.getSearchFieldValue()) {
-            return mid;
-         }
-         if (midElement.value + 1 < this.getSearchFieldValue()) {
-            return this.#binarySearch(mid + 1, endIndex);
-         } else {
-            return this.#binarySearch(startIndex, mid);
-         }
+         midElement.label.style.borderColor = "yellow";
+         setTimeout(() => {
+            midElement.label.style.borderColor = "black";
+            if (midElement.value + 1 == this.getSearchFieldValue()) {
+               midElement.label.style.borderColor = "green";
+               return mid;
+            } else {
+               if (midElement.value + 1 < this.getSearchFieldValue()) {
+                  for (var i = startIndex; i < mid + 1; i++) {
+                     const ignoredElement = this.stage[i]["node"];
+                     ignoredElement.label.style.borderColor = "red";
+                  }
+                  return this.#binarySearch(mid + 1, endIndex);
+               } else {
+                  for (var i = mid; i < endIndex; i++) {
+                     const ignoredElement = this.stage[i]["node"];
+                     ignoredElement.label.style.borderColor = "red";
+                  }
+                  return this.#binarySearch(startIndex, mid);
+               }
+            }
+         }, 1000);
       }
       return -1;
    }
