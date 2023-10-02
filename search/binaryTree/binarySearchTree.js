03b866b33e3c24749f44cf88a443b79fef4124bc
francistopher <francistopherrr@outlook.com>
2023-10-01 23:04:20 -0400

    UPDATE binary search tree index.html

---
+++ search/binaryTree/binarySearchTree.js
@@ -0,0 +1,8 @@
+class BinarySearchTree extends TreeNodeManager {
+   constructor() {
+      super(2);
+      // binary tree cant balance
+      this.hideBalanceButton();
+      this.showSearchButton();
+   }
+}
