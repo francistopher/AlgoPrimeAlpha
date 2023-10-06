396740b52de437b7078f6c57f928885af243b65c
francistopher <francistopherrr@outlook.com>
2023-10-06 11:37:05 -0400

    UPDATE binaryTreeNode.js slider to generate values to display

--- search/binaryTree/binarySearchTree.js
+++ search/binaryTree/binarySearchTree.js
@@ -3,5 +3,44 @@ class BinarySearchTree extends TreeNodeManager {
       super(2);
       // binary tree cant balance
       this.hideBalanceButton();
+      this.root;
+      this.#listenToAddGeneratedValue();
    }
+
+   #buildNodes() {}
+
+   /**
+    * Shuffles the nodes of the tree by
+    * unloading the element values into an array
+    * shuffles the array with the element values
+    * and rebuilds the tree with the shuffled elements
+    */
+   #shuffleNodes() {}
+
+   #listenToAddGeneratedValue() {
+      this.nodeCountSlider.addEventListener("input", () => {
+         this.#addGeneratedValue();
+      });
+   }
+
+   #addGeneratedValue() {
+      if (this.values.length < this.nodeCountSlider.value) {
+         const randommm = Math.floor(Math.random() * 50 + 1);
+         if (this.values.includes(randommm)) {
+            // console.log(randommm, "repeat");
+            this.#addGeneratedValue();
+         } else {
+            this.values.push(randommm);
+            // console.log(randommm, this.values, this.values.length, this.nodeCountSlider.value);
+         }
+      }
+   }
+
+   addNode() {
+      if (this.root == NaN) {
+         this.root = new TreeNode(this.maxChildren);
+      }
+   }
+
+   #createPath() {}
 }
