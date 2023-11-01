c3a46a012fd255ca1583d5f00d2b88332b652aa6
francistopher <francistopherrr@outlook.com>
2023-10-31 22:29:47 -0400

    CREATE GraphNode starting draft

---
+++ graphNode.js
@@ -0,0 +1,11 @@
+class GraphNode {
+   constructor() {
+      this.element = None;
+      this.#createElement();
+   }
+
+   #createElement() {
+      this.element = document.createElement("LABEL");
+      this.element.id = "node";
+   }
+}
