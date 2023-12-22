fe15c8b8f46302074d860a586c13950ce2ac468c
francistopher <francistopherrr@outlook.com>
2023-12-21 19:34:53 -0500

    CREATE node count slider

--- search/breadthFirst/graph.js
+++ search/breadthFirst/graph.js
@@ -8,18 +8,28 @@ class Graph {
       console.log("Hello World! This is Graph!");
       this.nodes = [];
       this.nodesCountSlider = NaN;
-      this.nodesSliderLabel = NaN;
+      this.nodesCountLabel = NaN;
       this.#setNodesCountLabel();
+      this.#setNodesCountSlider();
    }
 
    #setNodesCountLabel() {
-      this.nodesSliderLabel = document.createElement("label");
-      this.nodesSliderLabel.innerHTML = "Nodes Count Slider: ";
-      this.nodesSliderLabel.style.display = "inline";
-      document.body.appendChild(this.nodesSliderLabel);
+      this.nodesCountLabel = document.createElement("label");
+      this.nodesCountLabel.innerHTML = "Nodes Count Slider: ";
+      this.nodesCountLabel.style.display = "inline";
+      document.body.appendChild(this.nodesCountLabel);
    }
 
-   #setNodesCountSlider() {}
+   #setNodesCountSlider() {
+      this.nodeCountSlider = document.createElement("input");
+      this.nodeCountSlider.type = "range";
+      this.nodeCountSlider.value = 0;
+      this.nodeCountSlider.min = "0";
+      this.nodeCountSlider.max = "25";
+      this.nodeCountSlider.style.accentColor = "black";
+      this.nodeCountSlider.style.display = "inline-block";
+      document.body.appendChild(this.nodeCountSlider);
+   }
 
    #setPathsCountLabel() {}
 
