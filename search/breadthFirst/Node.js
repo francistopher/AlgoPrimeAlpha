8304b9bc3fbc07f5c46585e3edda2a0c75f85003
francistopher <francistopherrr@outlook.com>
2023-12-23 00:20:20 -0500

    UPDATE MyNode to be repositioned

--- search/breadthFirst/Node.js
+++ search/breadthFirst/Node.js
@@ -2,6 +2,8 @@ class MyNode {
    constructor(value) {
       console.log("Hello World! I am node!");
       this.element;
+      this.x;
+      this.y;
       this.#createElement(value);
    }
 
@@ -15,5 +17,23 @@ class MyNode {
 
    removeFromDocument() {
       this.element.remove();
+   }
+
+   reposition(x, y) {
+      console.log(typeof x, typeof y);
+      this.element.style.position = "absolute";
+      if (typeof x === Number) {
+         this.element.style.left = x + "vw";
+         console.log(x);
+         // console.log(x.substring(0, -2));
+      } else {
+         this.element.style.left = x;
+      }
+      if (typeof y === Number) {
+         this.element.style.top = y + "vh";
+         // console.log(y.substring(0, -2));
+      } else {
+         this.element.style.top = y;
+      }
    }
 }
