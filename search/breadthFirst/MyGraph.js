5a8ca5df8c71c69838d9eedf4e812d0eaa3ed563
francistopher <francistopherrr@outlook.com>
2023-12-24 01:27:34 -0500

    CREATE paths between nodes

--- search/breadthFirst/MyGraph.js
+++ search/breadthFirst/MyGraph.js
@@ -2,6 +2,7 @@ class Graph {
    constructor() {
       console.log("Hello World! This is Graph!");
       this.nodes = [];
+      this.paths = [];
       this.values = [];
       this.nodesCountSlider = NaN;
       this.nodesCountLabel = NaN;
@@ -36,6 +37,38 @@ class Graph {
    }
 
    /**
+    *
+    * @param {HTMLElement} nodeA
+    * @param {HTMLElement} nodeB
+    * generates a path from node A to node B
+    */
+   #generateNewPath(nodeA, nodeB) {
+      // get difference in x and y
+      const dx = nodeB.x - nodeA.x;
+      const dy = nodeB.y - nodeA.y;
+      // calculate distance
+      const distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+      // calculate angle of horizontal to path
+      const angleRadians = Math.atan2(dy, dx);
+      const angleDegrees = (angleRadians * 180) / Math.PI;
+      // create path
+      const newPath = document.createElement("label");
+      newPath.style.backgroundColor = "black";
+      newPath.style.position = "absolute";
+      newPath.style.width = distance + "vh";
+      newPath.style.height = "1vh";
+      newPath.style.left = nodeA.x + "vh";
+      newPath.style.top = nodeA.y + "vh";
+      newPath.style.transformOrigin = "top left";
+      newPath.style.marginLeft = "3vh";
+      newPath.style.marginTop = "3vh";
+      newPath.style.transform = "rotate(" + angleDegrees + "deg)";
+      newPath.style.zIndex = "1";
+      document.body.appendChild(newPath);
+      this.paths.push(newPath);
+   }
+
+   /**
     * Generates a new value, a value not in values
     */
    #addNewValue() {
@@ -55,6 +88,11 @@ class Graph {
          var newNode = new MyNode(this.#addNewValue());
          this.#randomlyPlaceNode(newNode);
          this.nodes.push(newNode);
+         const nodesSize = this.nodes.length;
+         if (nodesSize > 1) {
+            this.#generateNewPath(this.nodes[nodesSize - 2], this.nodes[nodesSize - 1]);
+            console.log(this.paths);
+         }
          if (this.nodes.length != this.nodesCountSlider.value) {
             this.#addNewNode();
          }
@@ -65,13 +103,14 @@ class Graph {
     * Randomly plots new node and assures it doesn't intersect with any other node
     */
    #randomlyPlaceNode(newNode) {
-      const xPos = Math.floor(Math.random() * 91) + 3;
+      const xPos =
+         ((Math.floor(Math.random() * 91) + 3) * window.screen.width) / window.screen.height;
       const yPos = Math.floor(Math.random() * 81) + 13;
       if (this.#isTooClose(xPos, yPos)) {
          //console.log("too closeee");
          this.#randomlyPlaceNode(newNode);
       } else {
-         newNode.reposition(xPos + "vw", yPos + "vh");
+         newNode.reposition(xPos + "vh", yPos + "vh");
       }
    }
 
@@ -83,7 +122,6 @@ class Graph {
          const node = this.nodes[i];
          const distance = Math.ceil(Math.sqrt(Math.pow(x - node.x, 2) + Math.pow(y - node.y, 2)));
          if (distance < 7) {
-            //console.log("too close");
             return true;
          }
       }
